3bf8be23ffb149c9231dc870a81190e929a88822
ravindrabotta <39752798+ravindrabotta@users.noreply.github.com>
2024-07-17 08:01:26 +0530

    added: arranging numbers in array

---
+++ SECTION_2/arrays1_exercises/ArrangeNumbersInAnArray.java
@@ -0,0 +1,29 @@
+package arrays1_exercises;
+
+import java.util.Scanner;
+public class ArrangeNumbersInAnArray {
+
+    public static int[] createArray(int size) {
+        int[] arr = new int[size];
+        int mid = size / 2;
+        int k = 1;
+        for(int i = 0; i <= mid; i++) {
+            arr[i] = k;
+            if (i < mid) {
+                k++;
+                arr[size - i - 1] = k;
+            }
+            k++;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        scan.close();
+        int[] arr = createArray(size);
+        for(int i = 0; i < size; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
